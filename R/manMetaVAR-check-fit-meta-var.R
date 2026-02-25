e3a11eb4a7f7ac34efeb975012d8cbe0e36bc3c7
jeksterslab <jeksterslab@users.noreply.github.com>
2026-02-25 19:39:13 +0000

    Update src to output generated at 3be6ba1

---
+++ R/manMetaVAR-check-fit-meta-var.R
@@ -0,0 +1,80 @@
+#' Check Replication - FitMetaVAR
+#'
+#' @details This function is executed via the `Check` function.
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @return The output is saved as an external file in `output_folder`.
+#'
+#' @inheritParams Template
+#'
+#' @export
+#' @keywords manMetaVAR check simulation
+CheckFitMetaVAR <- function(taskid,
+                            repid,
+                            output_folder,
+                            suffix) {
+  # Do not include default arguments here.
+  # Do not run on its own. Use the `Check` function.
+  fn_input <- SimFN(
+    output_type = "fit-meta-var-mx",
+    output_folder = output_folder,
+    suffix = suffix
+  )
+  tryCatch(
+    {
+      out <- metaDyn:::.CheckStatusCode(
+        model = readRDS(fn_input)$output
+      )
+      if (out > 0) {
+        cat(
+          paste(
+            "check",
+            "taskid:",
+            taskid,
+            "repid:",
+            repid,
+            "\n"
+          )
+        )
+        cat(
+          paste(
+            "\nStatus code:",
+            out,
+            "\n"
+          )
+        )
+      }
+    },
+    error = function(cond) {
+      message(paste("error:", "CheckFitMetaVAR"))
+      message("Here's the original error message:")
+      message(conditionMessage(cond))
+      cat(
+        paste(
+          "check",
+          "taskid:",
+          taskid,
+          "repid:",
+          repid,
+          "\n"
+        )
+      )
+    },
+    warning = function(cond) {
+      message(paste("error:", "CheckFitMetaVAR"))
+      message("Here's the original warning message:")
+      message(conditionMessage(cond))
+      cat(
+        paste(
+          "check",
+          "taskid:",
+          taskid,
+          "repid:",
+          repid,
+          "\n"
+        )
+      )
+    }
+  )
+}
---
+++ R/manMetaVAR-check-fit-mplus.R
@@ -0,0 +1,78 @@
+#' Check Replication - FitMplus
+#'
+#' @details This function is executed via the `Check` function.
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @return The output is saved as an external file in `output_folder`.
+#'
+#' @inheritParams Template
+#'
+#' @importFrom stats coef vcov
+#' @export
+#' @keywords manMetaVAR check simulation
+CheckFitMplus <- function(taskid,
+                          repid,
+                          output_folder,
+                          suffix) {
+  # Do not include default arguments here.
+  # Do not run on its own. Use the `Check` function.
+  fn_input <- SimFN(
+    output_type = "fit-mplus",
+    output_folder = output_folder,
+    suffix = suffix
+  )
+  tryCatch(
+    {
+      out <- grep(
+        pattern = "THE MODEL ESTIMATION TERMINATED NORMALLY",
+        x = readRDS(fn_input)$output$output
+      )
+      if (out == "") {
+        cat(
+          paste(
+            "check",
+            "taskid:",
+            taskid,
+            "repid:",
+            repid,
+            "\n"
+          )
+        )
+        cat(
+          "\nEstimation error\n"
+        )
+      }
+    },
+    error = function(cond) {
+      message(paste("error:", "CheckFitMplus"))
+      message("Here's the original error message:")
+      message(conditionMessage(cond))
+      cat(
+        paste(
+          "check",
+          "taskid:",
+          taskid,
+          "repid:",
+          repid,
+          "\n"
+        )
+      )
+    },
+    warning = function(cond) {
+      message(paste("error:", "CheckFitMplus"))
+      message("Here's the original warning message:")
+      message(conditionMessage(cond))
+      cat(
+        paste(
+          "check",
+          "taskid:",
+          taskid,
+          "repid:",
+          repid,
+          "\n"
+        )
+      )
+    }
+  )
+}
